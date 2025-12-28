f7dedb7d7c0395124e0a2ab71df3b201ddaea886
maggidev <maggidev@protonmail.com>
2025-12-28 06:07:51 -0300

    Adiciona/atualiza composer.lock para Vercel instalar dependências corretamente

--- api/router.php
+++ api/router.php
@@ -3,7 +3,7 @@
  * Router principal da API
  */
 
-require_once __DIR__ . '/vendor/autoload.php';
+require_once __DIR__ . '/../vendor/autoload.php';
 require_once __DIR__ . '/config/config.php';
 require_once __DIR__ . '/utils/Security.php';
 require_once __DIR__ . '/utils/Logger.php';
